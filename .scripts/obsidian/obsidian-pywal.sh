a2c3b9b8fe0a6c9adebd93242930a0dd80698483
Dung Ngo <quocdungngole@gmail.com>
2023-07-16 16:45:36 +0700

    pywal for obsidian but not working yet

---
+++ .scripts/obsidian/obsidian-pywal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+vault_dir="$1"
+
+if [[ -z ${vault_dir} ]];
+then
+    read -p "Enter Vault Directory: " vault_dir
+fi
+
+workdir="$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")"
+
+echo $workdir
+
+theme_dir="$vault_dir/.obsidian/themes/pywal"
+
+mkdir -p "$theme_dir"
+cp "$workdir/manifest.json" "$theme_dir/manifest.json"
+cp "$workdir/theme.css" "$theme_dir/theme.css"
+

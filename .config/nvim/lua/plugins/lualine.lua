e6d60636ce2df700f83b911b4c451128dfc48cbb
Dung Ngo <dung.ngo@eastagile.com>
2024-01-29 13:33:55 +0700

    configs that added icon for lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		opts = {
 			options = {
-				icons_enabled = false,
+				icons_enabled = true,
 				theme = 'auto',
 				component_separators = '|',
 				section_separators = '',

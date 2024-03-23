7aeab9b18c238e36f538caec70276d6ba2c22331
Dung Ngo <quocdungngole@gmail.com>
2024-03-23 12:18:56 +0700

    chore: remove unused plugin, make git load faster

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,12 +1,20 @@
 return {
-	-- Git related
-	'tpope/vim-fugitive',
-	'tpope/vim-rhubarb',
+	-- Based plugin to help working with git in the flow
+	{
+		'tpope/vim-fugitive',
+		event = 'VeryLazy',
+	},
+
+	-- Maybe useful when you have to check github back and forth
+	-- {
+	-- 	'tpope/vim-rhubarb',
+	-- 	event = 'VimEnter',
+	-- },
 
 	{ -- Adds git releated signs to the gutter, as well as utilities for managing changes
 		'lewis6991/gitsigns.nvim',
+		event = 'VeryLazy',
 		opts = {
-			-- See `:help gitsigns.txt`
 			signs = {
 				add = { text = '+' },
 				change = { text = '~' },
@@ -17,8 +25,10 @@ return {
 		},
 	},
 
+	-- Blame the shat out of everyone
 	{
 		'f-person/git-blame.nvim',
+		event = 'VeryLazy',
 		opts = {
 			delay = 1000,
 			date_format = '%r',

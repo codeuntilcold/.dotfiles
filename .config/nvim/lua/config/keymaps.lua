5021f8b5e46963f2bfc7b61e7f68ff8ef936856b
Dung Ngo <dung.ngo@eastagile.com>
2024-04-12 10:04:13 +0700

    feat: add some config from work

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,7 @@ vim.keymap.set('v', '<leader>y', '\"+y', { desc = '[Y]ank to system' })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set('n', '<leader>-', '<CMD>Explore<CR>')
 
 -- [[ Basic Keymaps ]]
 

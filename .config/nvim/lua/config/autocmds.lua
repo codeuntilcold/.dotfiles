19efbf2f973912c0ab2e03f91e9a93900f9f64d0
Dung Ngo <dung.ngo@eastagile.com>
2024-09-18 17:23:29 +0700

    feat: make lsp work with aptos too

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,11 +1,7 @@
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+-- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
+	callback = function() vim.highlight.on_yank() end,
+	group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
 	pattern = '*',
 })
 
@@ -40,18 +36,63 @@ vim.api.nvim_create_autocmd('BufEnter', {
 	end
 })
 
+-- For move projects
+local lsp_started = false
+
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = { '*.move' },
 	callback = function()
+		if lsp_started then return end
+
 		vim.lsp.set_log_level('off')
-		vim.lsp.start({
-			name = 'sui',
-			cmd = { vim.env.HOME .. '/.cargo/bin/move-analyzer' },
-			root_dir = vim.fs.dirname(vim.fs.find({ 'Move.toml' }, { upward = true })[1]),
-		})
+
+		local move_toml = vim.fs.find({ 'Move.toml' }, { upward = true })[1]
+		if not move_toml then
+			print("No Move.toml found")
+			return
+		end
+
+		local content = vim.fn.readfile(move_toml)
+		local repo_found = ''
+		for _, line in ipairs(content) do
+			if line:match("MystenLabs/sui") then
+				repo_found = 'sui'
+				break
+			end
+			if line:match("AptosFramework") then
+				repo_found = 'aptos'
+				break
+			end
+		end
+
+		local lsp_config = {
+			sui = { cmd = "move-analyzer" },
+			aptos = { cmd = "aptos-move-analyzer" }
+		}
+
+		if lsp_config[repo_found] then
+			local cmd = vim.fn.exepath(lsp_config[repo_found].cmd)
+			if cmd == "" then
+				print(string.format("%s not found in PATH", lsp_config[repo_found].cmd))
+				return
+			end
+
+			vim.lsp.stop_client(vim.lsp.get_active_clients())
+			vim.lsp.start({
+				name = repo_found,
+				cmd = { cmd },
+				root_dir = vim.fs.dirname(move_toml),
+			})
+
+			lsp_started = true
+			print(string.format("%s LSP started", repo_found))
+		else
+			print("Unknown Move project type")
+		end
 	end
 })
 
+-- Enable lsp keymaps
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(args)
 		local nmap = function(keys, func, desc)

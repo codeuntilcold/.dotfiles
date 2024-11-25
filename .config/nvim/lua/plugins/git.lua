ec7c19a516f80b2a715a0555512a01bcc2ddaf58
Dung Ngo <dung.ngo@eastagile.com>
2024-11-25 13:51:51 +0700

    feat: feels good to add debugprint and json decode

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,19 +4,40 @@ return {
         event = 'VeryLazy',
     },
 
-    { -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    {
         'lewis6991/gitsigns.nvim',
         event = 'VeryLazy',
-        opts = {},
-    },
-
-    -- Blame the shat out of everyone
-    {
-        'f-person/git-blame.nvim',
-        event = 'VeryLazy',
         opts = {
-            delay = 1000,
-            date_format = '%r',
+            current_line_blame = true,
+            on_attach = function()
+                local gitsigns = require('gitsigns')
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map('n', ']c', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ ']c', bang = true })
+                    else
+                        gitsigns.nav_hunk('next')
+                    end
+                end, { desc = 'Go to next git change'})
+
+                map('n', '[c', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ '[c', bang = true })
+                    else
+                        gitsigns.nav_hunk('prev')
+                    end
+                end, { desc = 'Go to prev git change'})
+
+                -- Actions
+                map('n', '<leader>gp', gitsigns.preview_hunk, { desc = '[G]itsigns [P]review Hunk'})
+            end
         },
     },
 }

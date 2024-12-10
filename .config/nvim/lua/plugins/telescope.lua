31a527e7f74db68ee533c4be145c14a7dd0ddfa0
Dung Ngo <dung.ngo@eastagile.com>
2024-12-10 15:08:33 +0700

    update various configuration files and scripts

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,12 @@
--- Fuzzy Finder (files, lsp, etc)
 return {
     {
         'nvim-telescope/telescope.nvim',
-        event = 'VeryLazy',
         version = '*',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        },
         config = function()
-            -- [[ Configure Telescope ]]
-            -- See `:help telescope` and `:help telescope.setup()`
             local telescope = require('telescope')
             local builtin = require('telescope.builtin')
 
@@ -22,49 +21,36 @@ return {
                 },
             }
 
-            -- Enable telescope fzf native, if installed
-            pcall(telescope.load_extension, 'fzf')
+            local dropdown = require('telescope.themes').get_dropdown { winblend = 10, previewer = false }
 
-            -- See `:help telescope.builtin`
-            vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
-            vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-            vim.keymap.set('n', '<leader>/', function()
-                -- You can pass additional configuration to telescope to change theme, layout, etc.
-                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-                    winblend = 10,
-                    previewer = false,
-                })
-            end, { desc = '[/] Fuzzily search in current buffer' })
+            local function nmap(key, func, desc)
+                vim.keymap.set('n', key, func, { desc = desc })
+            end
 
-            vim.keymap.set('n', '<leader>sf', function ()
-                builtin.find_files(require('telescope.themes').get_dropdown {
-                    previewer = false,
-                })
-            end , { desc = '[S]earch [F]iles' })
-            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-            vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-            vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-            vim.keymap.set('n', '<leader>sn', function()
-                builtin.find_files { cwd = vim.fn.stdpath 'config' }
-            end, { desc = '[S]earch [N]eovim files' })
+            nmap('<leader>?', builtin.oldfiles, '[?] Find recently opened files')
+            nmap('<leader><space>', builtin.buffers, '[ ] Find existing buffers')
+            nmap('<leader>/', function() builtin.current_buffer_fuzzy_find(dropdown) end, '[/] Fuzzily search in current buffer')
 
-            local actions = require('telescope.actions')
-            local action_state = require('telescope.actions.state')
+            nmap('<leader>sf', function () builtin.find_files(dropdown) end, '[S]earch [F]iles')
+            nmap('<leader>sh', builtin.help_tags, '[S]earch [H]elp')
+            nmap('<leader>sg', builtin.live_grep, '[S]earch by [G]rep')
+            nmap('<leader>sd', builtin.diagnostics, '[S]earch [D]iagnostics')
+            nmap('<leader>sr', builtin.resume, '[S]earch [R]esume')
 
             vim.keymap.set('n', '<leader>gl', function()
                 builtin.git_commits({
                     attach_mappings = function(_, map)
                         map('i', '<CR>', function(prompt_bufnr)
-                            local selection = action_state.get_selected_entry()
-                            actions.close(prompt_bufnr)
+                            local selection = require('telescope.actions.state').get_selected_entry()
+                            require('telescope.actions').close(prompt_bufnr)
                             local commit_hash = selection.value
                             local Job = require('plenary.job')
                             local output = Job:new({
                                 command = "git",
                                 args = { "show", "-s", "--format=%B", commit_hash },
                             }):sync()
+                            if not output then return end
+
                             vim.api.nvim_put(output, 'l', false, false)
                         end)
                         return true
@@ -72,13 +58,5 @@ return {
                 })
             end, { desc = '[G]o get [L]ast commit' })
         end
-    },
-
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-            return vim.fn.executable 'make' == 1
-        end,
     },
 }

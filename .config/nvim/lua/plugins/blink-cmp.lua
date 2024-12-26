a6721231a576406527cc7998f3b3496e1b9a1925
Dung Ngo <dung.ngo@eastagile.com>
2024-12-26 10:48:15 +0700

    feat: blink-cmp for dadbod

---
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,20 @@
+return {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+
+    -- use a release tag to download pre-built binaries
+    version = '*',
+
+    opts = {
+        keymap = { preset = 'default' },
+        appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = 'mono'
+        },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'dadbod' },
+        },
+        signature = { enabled = true },
+    },
+    opts_extend = { "sources.default" },
+}
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -56,7 +56,8 @@ return {
 
         -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
         local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+        -- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+        capabilities = require('blink.cmp').get_lsp_capabilities()
 
         -- Setup mason so it can manage external tooling
         require('mason').setup()

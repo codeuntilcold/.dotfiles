20ea731eb654bd3b5e6cd8be08217908cbde308d
Dung Ngo <dung.ngo@eastagile.com>
2025-01-10 10:58:53 +0700

    chore: minor update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -23,4 +23,22 @@ config.window_padding = {
   bottom  =  16,
 }
 
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'zenwritten_dark'
+  else
+    return 'zenwritten_light'
+  end
+end
+
+wezterm.on('window-config-reloaded', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
+
 return config

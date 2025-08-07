9038cd12cb815169b6aed5f293efa3ee1a241494
Dung Ngo <quocdungngole@gmail.com>
2025-08-07 23:43:51 +0700

    feat: switch to light mode since i'm sane

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,4 @@
-local function getcolorscheme()
-    if os.getenv("USER") == "qd" or os.getenv("SSH_CONNECTION") ~= nil then
-        return
-    end
-    vim.cmd.set "background=light"
-    -- vim.cmd.colorscheme "zenwritten"
-end
+vim.cmd.set "background=light"
 
 return {
     -- Transparent background
@@ -17,9 +11,9 @@ return {
         "catppuccin/nvim",
         enable = false,
         name = "catppuccin",
-        config = function()
-            getcolorscheme()
-        end
+        -- config = function()
+        --     getcolorscheme()
+        -- end
     },
 
     {
@@ -27,12 +21,12 @@ return {
         enable = false,
         lazy = false,
         priority = 1000,
-        config = function()
-            vim.g.zenwritten_compat = 1
-            vim.g.zenwritten_lightness = 'dim'
-            vim.g.zenwritten_darken_comments = 45
-            vim.g.zenwritten_transparent_background = true
-            getcolorscheme()
-        end
+        -- config = function()
+        --     vim.g.zenwritten_compat = 1
+        --     vim.g.zenwritten_lightness = 'dim'
+        --     vim.g.zenwritten_darken_comments = 45
+        --     vim.g.zenwritten_transparent_background = true
+        --     getcolorscheme()
+        -- end
     },
 }

6c9d4b38c8cd89360fefe3d1315e6d5f3f7f7fb1
Dung Ngo <dung.ngo@eastagile.com>
2025-09-29 13:46:38 +0700

    feat(oil): toggle ls-like details

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,7 @@ return {
     'stevearc/oil.nvim',
     opts = {
         default_file_explorer = true,
+        columns = { "icon" },
         delete_to_trash = true,
         view_options = {
             show_hidden = true,
@@ -12,6 +13,15 @@ return {
         },
         win_options = {
             wrap = true,
+        },
+        keymaps = {
+            ["gd"] = function ()
+                if #require("oil.config").columns == 1 then
+                    require("oil").set_columns({ "icon", "premissions", "size", "mtime" })
+                else
+                    require("oil").set_columns({ "icon" })
+                end
+            end
         },
     }
 }
